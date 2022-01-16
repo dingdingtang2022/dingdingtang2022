5c96f43c567b9186653009e4c2d669de47683af5
dingdingtang2022 <1255981621@qq.com>
2022-01-16 20:00:03 +0800

    first version, cake

---
+++ config.js
@@ -0,0 +1,24 @@
+var config = {
+    texts: [
+        "送给我",
+        "亲爱的宝宝",
+        "今天是你的生日",
+        "生日快乐",
+        "YAMI~~",
+    ],
+
+    imgs: {
+        "亲爱的宝宝": "./imgs/xiaokeai.jpeg",
+        // "今天是你的生日": "./imgs/birthday.jpg",
+    },
+    desc: {
+        turn_on: "屋里太黑了，开下灯",
+        play: "放点音乐吧",
+        bannar_coming: "好像少了点什么",
+        balloons_flying: "摆点气球吧，气氛跟上",
+        cake_fadein: "蛋糕不能少",
+        light_candle: "蜡烛要点上",
+        wish_message: "许愿吧，祝你生日快乐！",
+        story: "还有些话想对你说",
+    }
+};
